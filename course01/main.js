6302975850a2f41434eee940486ea058257deb9f
Adrian Moldovan <ma.moldovan@gmail.com>
2024-09-15 09:48:05 +0300

    adds the first JS program

---
+++ course01/main.js
@@ -0,0 +1,14 @@
+// debugger;
+
+// DATA INPUT SECTION
+let firstNr = prompt('Input the first number');
+firstNr = Number(firstNr);
+
+let secondNr = prompt('Input the second number');
+secondNr = Number(secondNr);
+
+// PROCESSING SECTION
+let sum = firstNr + secondNr;
+
+// DATA OUTPUT SECTION
+alert(`Sum is: ${sum}!`);
