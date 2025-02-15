6b10e9fefd25eee2af2fe367044b1802748a7bbe
Paul Negoescu <paul.negoescu@nagarro.com>
2025-02-15 11:41:12 +0200

    End of course 4, todos crud and react router

--- react/src/features/Todos/TodoItem.jsx
+++ react/src/features/Todos/TodoItem.jsx
@@ -1,10 +1,34 @@
-export function TodoItem({data}) {
+import { HiTrash } from 'react-icons/hi2';
+
+import styles from './Todos.module.css';
+
+export function TodoItem({ data, onDelete, onComplete }) {
+  function handleDeleteClick() {
+    onDelete(data);
+  }
+
+  function handleComplete(e) {
+    const isCompleted = e.target.checked;
+    onComplete(data.id, isCompleted);
+  }
+
   return (
     <li>
       <label>
-        <input type="checkbox" defaultChecked={data.completed} />
+        <input
+          type="checkbox"
+          defaultChecked={data.completed}
+          onChange={handleComplete}
+        />
         {data.title}
-      </label>
+      </label>{' '}
+      <button
+        type="button"
+        className={styles.deleteButton}
+        onClick={handleDeleteClick}
+      >
+        <HiTrash />
+      </button>
     </li>
-  )
+  );
 }
